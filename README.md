2dc1197f1e1e5adaf507e537687e8aceb3bda0d9
William de Witt <sasilverain@hotmail.com>
2024-06-19 19:45:38 +0200

    feat: Can do things.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # uuill
+
+Challenging Project #1: Build a Text Editor.
+
+So this is going to be my attempt at building a text editor.
+
+Lets go back in time a little and make the size of a "page" 80 x 12 characters. The same size as the IBM punch cards.
+
+https://austinhenley.com/blog/challengingprojects.html
+
---
+++ src/Uuill.App/Program.cs
@@ -0,0 +1,96 @@
+﻿const int pageHeight = 10;
+const int pageWidth = 10;
+
+Console.Clear();
+
+var lin = 0;
+var col = 0;
+var page = new char[pageHeight, pageWidth];
+Array.Clear(page);
+
+do
+{
+  var key = Console.ReadKey(true);
+
+  if (IsArrowKey(key))
+  {
+    MoveCursor(key);
+  }
+  else
+  {
+    PrintKey(key.KeyChar);
+  }
+}
+while (lin < pageHeight);
+
+void MoveCursor(ConsoleKeyInfo key)
+{
+  if (key.Key == ConsoleKey.LeftArrow)
+  {
+    if (lin == 0 && col == 0)
+    {
+      PrintCursor();
+      return;
+    }
+
+    if (col == 0 && lin > 0)
+    {
+      lin--;
+      col = pageWidth;
+    }
+    else if (col > 0)
+    {
+      col--;
+    }
+
+    PrintCursor();
+    Console.SetCursorPosition(col, lin);
+  }
+}
+
+bool IsArrowKey(ConsoleKeyInfo key)
+{
+  // LeftArrow = 37
+  return key.Key == ConsoleKey.LeftArrow;
+}
+
+Console.WriteLine("Thanks for playing.");
+SaveToFile();
+
+void SaveToFile(string fileName = "temp.txt")
+{
+  for(int i = 0; i < pageHeight; i++)
+  {
+    for(int j = 0; j < pageWidth; j++)
+    {
+      File.AppendAllText($"./src/Uuill.App/{fileName}", page[i,j].ToString());
+    }
+  }
+}
+
+void PrintKey(char key)
+{
+  page[lin, col] = key;
+  Console.Write(key);
+
+  col++;
+
+  if (col == pageWidth)
+  {
+    col = 0;
+    Console.SetCursorPosition(col, lin);
+    // Console.WriteLine();
+    lin++;
+  }
+
+  PrintCursor();
+}
+
+void PrintCursor()
+{
+  Console.SetCursorPosition(0, pageHeight + 2);
+  Console.Write($"line:   ; column   ");
+  Console.SetCursorPosition(0, pageHeight + 2);
+  Console.Write($"line: {lin}; column {col}");
+  Console.SetCursorPosition(col, lin);
+}
